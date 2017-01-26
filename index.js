e22aea6bd90856802b84c06bf6efe45dc2982c7e
kovalexis <alexis.giguere-joannette@mail.mcgill.ca>
2017-01-25 19:23:15 -0500

    k

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ function webCheck() {
       comp251: COMP251
     }
   }, function(error, response, body) {
+    console.log(body);
+    sendMessage(senderID, {text: "Hello dude"});
     if (body.split(':')[0] == "new") {
       var title = body.split(':')[1];
       var course = body.split(':')[2];
